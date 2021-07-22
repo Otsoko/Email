daf0df438a44f17a5dfe8912a0a442fe5c85e335
Moritz Bergmann <mobergmann@pm.me>
2021-07-22 09:33:40 +0200

    added include folder structure

--- src/email.cpp
+++ src/email.cpp
@@ -1,102 +1,109 @@
-#include "email.h"
+#include "../include/email.h"
 #include <iostream>
-#include <netdb.h>         /* gethostbyname  */
-#include <netinet/in.h>    /* htons          */
+#include <netdb.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
-#include <string.h>
-#include <stdio.h>
+#include <cstring>
+#include <cstdio>
 #include <unistd.h>
 
-using namespace std;
+namespace libemail {
 
-Email* Email::instance = NULL;
+    Email *Email::instance = nullptr;
 
-Email::Email() {}
+    Email::Email() {}
 
-Email::~Email() {}
+    Email::~Email() {}
 
-Email* Email::Instance() {
-	if(instance == NULL) {
-		instance = new Email();
-	}
-	return instance;
-}
+    Email *Email::Instance() {
+        if (!instance) {
+            instance = new Email();
+        }
+        return instance;
+    }
 
-void Email::Release() {
-	if(instance) {
-		delete instance;
-	}
-	instance = NULL;
-}
+    void Email::Release() {
+        delete instance;
+        instance = nullptr;
+    }
 
-void Email::writeSocket(const int sock, const char *str, const char *arg) {
-	char buf[4096];
+    void Email::writeSocket(const int sock, const char *str, const char *arg) {
+        char buf[4096];
 
-	if(arg != NULL)
-		snprintf(buf, sizeof(buf), str, arg);
-	else
-		snprintf(buf, sizeof(buf), str, NULL);
+        if (arg) {
+            snprintf(buf, sizeof(buf), str, arg);
+        } else {
+            snprintf(buf, sizeof(buf), str, NULL);
+        }
 
-	send(sock, buf, strlen(buf), 0);
-}
+        send(sock, buf, strlen(buf), 0);
+    }
 
-int Email::sendmail(const char *from, std::vector<std::string> recipients, const char *subject, const char *body, const char *hostname, const int port) {
-	struct hostent *host;
-	struct sockaddr_in saddr_in;
-	int sock = 0;
-	char buf[256];
+    int Email::sendmail(
+            const char *from,
+            std::vector<std::string> recipients,
+            const char *subject,
+            const char *body,
+            const char *hostname,
+            const int port) {
+        struct hostent *host;
+        struct sockaddr_in saddr_in;
+        int sock = 0;
+        char buf[256];
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	host = gethostbyname(hostname);
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        host = gethostbyname(hostname);
 
-	saddr_in.sin_family      = AF_INET;
-	saddr_in.sin_port        = htons((u_short)port);
-	saddr_in.sin_addr.s_addr = 0;
+        saddr_in.sin_family = AF_INET;
+        saddr_in.sin_port = htons((u_short) port);
+        saddr_in.sin_addr.s_addr = 0;
 
-	memcpy((char*) & (saddr_in.sin_addr), host->h_addr, host->h_length);
+        memcpy((char *) &(saddr_in.sin_addr), host->h_addr, host->h_length);
 
-	if(connect(sock, (struct sockaddr*)&saddr_in, sizeof(saddr_in)) == -1) {
-		return -2;
-	}
+        if (connect(sock, (struct sockaddr *) &saddr_in, sizeof(saddr_in)) == -1) {
+            return -2;
+        }
 
-	writeSocket(sock, "HELO %s\n",       from);    // greeting
-	recv(sock, buf, 254, 0);
-	printf("\nRec: %s", buf);
-	writeSocket(sock, "MAIL FROM: %s\n", from);    // from
-	recv(sock, buf, 254, 0);
-	printf("\nRec: %s", buf);
-	
-	for(int i = 0; i < (int)recipients.size(); i++){
-		writeSocket(sock, "RCPT TO: %s\n",   recipients.at(i).c_str()); // to
-		//printf("RCPT TO: %s\n", recipients.at(i).c_str());
-		recv(sock, buf, 254, 0);
-	}
-	
-	printf("\nRec: %s", buf);
-	writeSocket(sock, "DATA\n",          NULL);    // begin data
-	recv(sock, buf, 254, 0);
-	printf("\nRec: %s", buf);
+        writeSocket(sock, "HELO %s\n", from); // greeting
+        recv(sock, buf, 254, 0);
+        printf("\nRec: %s", buf);
+        writeSocket(sock, "MAIL FROM: %s\n", from); // from
+        recv(sock, buf, 254, 0);
+        printf("\nRec: %s", buf);
 
-	// next comes mail headers
-	writeSocket(sock, "From: %s\n",      from);
-	
-	for(int i = 0; i < (int)recipients.size(); i++){
-		writeSocket(sock, "To: %s\n",   recipients.at(i).c_str()); // to
-		//printf("To: %s\n", recipients.at(i).c_str());
-	}
-	
-	writeSocket(sock, "Subject: %s\n",   subject);
+        for (auto &recipient : recipients) {
+            writeSocket(sock, "RCPT TO: %s\n", recipient.c_str()); // to
+            //printf("RCPT TO: %s\n", recipients.at(i).c_str());
+            recv(sock, buf, 254, 0);
+        }
 
-	writeSocket(sock, "\n",              NULL);
+        printf("\nRec: %s", buf);
+        writeSocket(sock, "DATA\n", NULL); // begin data
+        recv(sock, buf, 254, 0);
+        printf("\nRec: %s", buf);
 
-	writeSocket(sock, "%s\n",            body);    // data
+        // next comes mail headers
+        writeSocket(sock, "From: %s\n", from);
 
-	writeSocket(sock, ".\n",             NULL);    // end data
-	recv(sock, buf, 254, 0);
-	printf("\nRec: %s", buf);
-	writeSocket(sock, "QUIT\n",          NULL);    // terminate
+        for (auto &recipient : recipients) {
+            writeSocket(sock, "To: %s\n", recipient.c_str()); // to
+            //printf("To: %s\n", recipients.at(i).c_str());
+        }
 
-	close(sock);
+        writeSocket(sock, "Subject: %s\n", subject);
 
-	return 0;
-}
+        writeSocket(sock, "\n", nullptr);
+
+        writeSocket(sock, "%s\n", body); // data
+
+        writeSocket(sock, ".\n", nullptr); // end data
+        recv(sock, buf, 254, 0);
+        printf("\nRec: %s", buf);
+        writeSocket(sock, "QUIT\n", nullptr); // terminate
+
+        close(sock);
+
+        return 0;
+    }
+
+} // namespace libemail
