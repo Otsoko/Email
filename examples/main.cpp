a1323696928d09e41f6ecb1ad5dfb6caeabffda6
Moritz Bergmann <mobergmann@pm.me>
2021-07-22 09:39:07 +0200

    updated example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <vector>
-#include "email.h"
+#include "../include/email.h"
+
+using libemail::Email;
 
 int main() {
-	Email *email = Email::Instance();
-	std::vector<std::string> recipients;
-	recipients.push_back("pepe@mailinator.com");
-	recipients.push_back("pepa@mailinator.com");
-	
-	int ret = email->sendmail(
-        "hola@hotmail.com",                              /* from     */
-        recipients,                                      /* to       */
-        "Mail subject",                                  /* subject  */
-        "Mail body.\n\nCan contain linebreaks.\n\nBye.", /* body     */
-        "localhost",                                     /* hostname */
-        25                                               /* port     */
+    Email *email = Email::Instance();
+    std::vector<std::string> recipients;
+    recipients.push_back("pepe@mailinator.com");
+    recipients.push_back("pepa@mailinator.com");
+
+    int ret = email->sendmail(
+            "hola@hotmail.com",                              /* from     */
+            recipients,                                      /* to       */
+            "Mail subject",                                  /* subject  */
+            "Mail body.\n\nCan contain linebreaks.\n\nBye.", /* body     */
+            "localhost",                                     /* hostname */
+            25                                               /* port     */
     );
 
-    if (ret != 0)
-		std::cerr << "Failed to send mail (code: " << ret << ")" << std::endl;
-    else
-		std::cout << "Mail successfully sent" << std::endl;
-	
-	email->Release();
-	return ret;
+    if (ret != 0) {
+        std::cerr << "Failed to send mail (code: " << ret << ")" << std::endl;
+    } else {
+        std::cout << "Mail successfully sent" << std::endl;
+    }
+
+    email->Release();
+
+    return ret;
 }
